edb8220327cd1503ddbfd79bbf9de4e69a094bfd
justicefriday <126066170+justicefriday@users.noreply.github.com>
2025-07-31 15:23:06 +0100

    Add CartContext file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { CartProvider } from './context/CartContext.jsx';
+import { CartProvider } from './Context/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
